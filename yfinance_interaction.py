af75b1b885b5784ea74bd393edab6f8bfc1d2e31
Matías Mingo <mamingo@uc.cl>
2023-09-29 17:27:51 -0300

    feat_ finished

--- yfinance_interaction.py
+++ yfinance_interaction.py
@@ -10,15 +10,18 @@ def get_symbol_info(symbol):
 
 def get_price_change(symbol, datetime_obj):
     first_date_str = datetime_obj.strftime("%Y-%m-%d")
-    second_day = datetime_obj + timedelta(days=1)
-    second_day_str = second_day.strftime("%Y-%m-%d")
-    ticker = yf.Ticker(symbol)
-
-    # Get historical data for the specified date
-    historical_data = ticker.history(period="1h", start=first_date_str, end=second_day_str)
-    closing_price = historical_data["Close"][0]
-    lowest_price = historical_data["Low"][0]
-    opening_price = historical_data["Open"][0]
-    highest_price = historical_data["High"][0]
-    percentage_change = ((closing_price/opening_price) - 1)*100
-    return percentage_change
+    current_datetime = datetime.now()
+    time_difference = current_datetime - datetime_obj
+    difference_in_days = time_difference.days
+    if difference_in_days >= 7:
+        second_day = datetime_obj + timedelta(days=7)
+        second_day_str = second_day.strftime("%Y-%m-%d")
+        ticker = yf.Ticker(symbol)
+        # Get historical data for the specified date
+        historical_data = ticker.history(period="1d", start=first_date_str, end=second_day_str)
+        closing_price = historical_data["Close"][0]
+        opening_price = historical_data["Open"][0]
+        percentage_change = ((closing_price/opening_price) - 1)*100
+        return percentage_change
+    else:
+        return None
--- zacks_scraper.py
+++ zacks_scraper.py
@@ -21,9 +21,6 @@ def write_json(data):
     with open(JSON_FILE, "w") as json_file:
         json.dump(data, json_file)
 
-def detect_stock_symbol(text):
-    pass
-
 def get_datetime(date_string):
     date_format = "%B %d,%Y"
     date_obj = datetime.strptime(date_string, date_format)
@@ -41,23 +38,27 @@ def scrape_content():
 
         # Extract the content within the div element
         for div_element in div_elements:
-            if div_element:
-                byline = div_element.find('p', class_='byline')
-                publish_date = byline.find('time').text
-                publish_datetime = get_datetime(" ".join(byline.find('time').text.split()[-2:]))
-                teaser = div_element.find('p', class_='teaser').text
-                teaser_elems = teaser.split()
-                if teaser_elems[2] == 'delivered' and teaser_elems[3] == 'earnings':    
-                    symbol = teaser_elems[1].strip("(").strip(")")
-                    earnings = teaser_elems[8].strip("%")
-                    revenue = teaser_elems[10][:-1][:-1]
-                    market_cap, industry = get_symbol_info(symbol)
-                    percentage_change = round(get_price_change(symbol, publish_datetime),2)
-                    data_structure = [[symbol, earnings, revenue, percentage_change, market_cap, industry]]
-                    write_to_csv(data_structure)
-            else:
-                print("Div element with class 'listitem' not found.")
-            
-            
+            try:
+                if div_element:
+                    byline = div_element.find('p', class_='byline')
+                    publish_date = byline.find('time').text
+                    publish_datetime = get_datetime(" ".join(byline.find('time').text.split()[-2:]))
+                    teaser = div_element.find('p', class_='teaser').text
+                    teaser_elems = teaser.split()
+                    if teaser_elems[2] == 'delivered' and teaser_elems[3] == 'earnings':    
+                        symbol = teaser_elems[1].strip("(").strip(")")
+                        earnings = teaser_elems[8].strip("%")
+                        revenue = teaser_elems[10][:-1][:-1]
+                        market_cap, industry = get_symbol_info(symbol)
+                        percentage_change = get_price_change(symbol, publish_datetime)
+                        if percentage_change != None:
+                            percentage_change = round(percentage_change, 2)
+                            data_structure = [[symbol, earnings, revenue, percentage_change, market_cap, industry]]
+                            write_to_csv(data_structure)
+                else:
+                    print("Div element with class 'listitem' not found.")
+            except Exception as e:
+                print(e)
+
 
 scrape_content()
